2169677041599d10cde97b9a42d316a8b3da83ad
natwPine <natw.mezza@hotmail.fr>
2022-01-28 23:14:36 +0100

    NMezza - [Feature] Interface

---
+++ Assets/01_Scripts/02_Enemies/BaseEnemyBehaviours.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyBehaviours : MonoBehaviour, IDamagable
+{
+    public void Dead()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsDead()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
