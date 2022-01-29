ed431a2d4bfec972860d1d2249766f9e922b19ab
natwPine <natw.mezza@hotmail.fr>
2022-01-29 01:22:10 +0100

    NMezza - Prefabs

---
+++ Assets/01_Scripts/BulletBehaviours.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviours : MonoBehaviour
+{
+
+    private Vector3 m_MoveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
