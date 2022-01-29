e781725cc73c6d9e6ac6abe2727fff63301091b0
natwPine <natw.mezza@hotmail.fr>
2022-01-29 01:47:18 +0100

    NMezza - bullet

--- Assets/01_Scripts/BulletBehaviours.cs
+++ Assets/01_Scripts/BulletBehaviours.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class BulletBehaviours : MonoBehaviour
 {
+    private Rigidbody rb;
 
-    private Vector3 m_MoveDirection;
+    [SerializeField] private Vector3 m_MoveDirection;
+    [SerializeField] private float m_Speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        LaunchBullet();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void LaunchBullet()
     {
-        
+        rb.velocity = m_MoveDirection * m_Speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.contacts[0].normal);
+        m_MoveDirection = collision.contacts[0].normal;
+        LaunchBullet();
     }
 }
